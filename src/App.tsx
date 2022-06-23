d7ca894a8ae81b317ef8caa61ba8b314582abb59
Christian Chang <christianchangthomaz@gmail.com>
2022-06-23 18:40:33 -0300

    Adding Video Section and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,17 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react"
+import { ApolloProvider, gql, useQuery } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 import { client } from "./lib/apollo";
-import { Event } from "./pages/Event";
+import { Router } from "./Router";
 
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 function App() {
-
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response => {
-  //     console.log(response.data);
-  //   })
-  // }, []);
-
-  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
   return (
-    // <h1 className="text-2xl font-bold text-violet-500">Hello World!</h1>
-    // <ul>
-    //   {data?.lessons.map(lesson => {
-    //     return <li key={lesson.id}>{lesson.title}</li>
-    //   })}
-    // </ul>
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   )
 }
