5d249345518e0e878006653bd5b6eab49fd4aa27
Christian Chang <christianchangthomaz@gmail.com>
2022-06-24 21:53:54 -0300

    Adding Home page and Subscribing using GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4p2go4t1m0801z49rhq8btv/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
